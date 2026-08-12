73f408d538222c879ea7dbaa87a1b9a96fc8ab21
aqu3180 <140891554+idiotf@users.noreply.github.com>
2026-08-12 19:19:00 +0900

    file-select.tsx 만들기

---
+++ apps/web/components/file-select.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import { useCallback } from 'react'
+import { ImageIcon } from 'lucide-react'
+import { FileSelectZone } from './그그그뭐더라'
+
+export interface ImageFile {
+  file: File
+  url: string
+}
+
+export interface ImageSelectZoneProps {
+  image: ImageFile | undefined
+  onImageChange: (file: File | undefined) => void
+}
+
+export function ImageSelectZone({
+  image,
+  onImageChange,
+}: ImageSelectZoneProps) {
+  const onFileSelect = useCallback(
+    (list: FileList) => {
+      const file = list[0]
+      if (!file) return
+      onImageChange(file)
+    },
+    [onImageChange]
+  )
+
+  return (
+    <FileSelectZone
+      accept={['image/*']}
+      selected={!!image}
+      onFileSelect={onFileSelect}
+    >
+      {image ? (
+        <Image
+          src={image.url}
+          alt=''
+          width={960}
+          height={540}
+          unoptimized
+        />
+      ) : (
+        <>
+          <ImageIcon />
+          이미지를 드롭하거나 선택
+        </>
+      )}
+    </FileSelectZone>
+  )
+}
